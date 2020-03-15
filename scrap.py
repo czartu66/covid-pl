cec5602160fd56a0aef059a6d288ee4c6e99c8f4
czartu66 <a.czartoryjski@gmail.com>
2020-03-16 00:30:09 +0100

    added flask

--- scrap.py
+++ scrap.py
@@ -1,12 +1,21 @@
 import pandas as pd
-url = 'https://raw.githubusercontent.com/dtandev/coronavirus/master/data/CoronavirusPL%20-%20Timeseries.csv'
-df = pd.read_csv(url, error_bad_lines=False)
+from flask import Flask, render_template
 
-# All infections
-numOfRows = df.shape[0]
-print('Number of people infected: ', numOfRows)
+app = Flask(__name__)
 
-# Check how many people infected in Lodz
 
-count = df['City'].value_counts()
-print(count)
+@app.route('/')
+def get_data():
+    url = 'https://raw.githubusercontent.com/dtandev/coronavirus/master/data/CoronavirusPL%20-%20Timeseries.csv'
+    df = pd.read_csv(url, error_bad_lines=False)
+
+    # All infections
+    total_cases = df.shape[0]
+
+    count = df['City'].value_counts()
+    count_dataframe = pd.DataFrame(count)
+    return render_template('get_data.html', data=count_dataframe, total_cases=total_cases)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
