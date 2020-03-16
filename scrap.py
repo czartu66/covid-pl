9b657db007a83923ce57374d0b3e5e08ba4e6b1d
czartu66 <a.czartoryjski@gmail.com>
2020-03-16 01:44:26 +0100

    add requirements

--- scrap.py
+++ scrap.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from flask import Flask, render_template
+import logging
 
 app = Flask(__name__)
 
@@ -19,3 +20,19 @@ def get_data():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_flex_quickstart]
